e48273f5e434ec3698ae1c12449ac1d7589015b4
bbk-muelling <24-hbfi1.muelling@bbk.schule>
2025-05-14 12:22:20 +0200

    Aufgabe gelöst

--- leapYear.py
+++ leapYear.py
@@ -1,7 +1,12 @@
 
 def isLeapYear(year):
     # Hier müssen Sie erweitern ...
-    return False
+    if ( year % 4 == 0  and year % 100 != 0 ):
+        return True
+    elif ( year % 400 == 0 ):
+        return True
+    else: 
+        return False
 
 def main():
     isLeapYear(2025)
